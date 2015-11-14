92ff2ea4af5513b0ca21771a9c2a792bee1931d3
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-11-14 13:24:12 -0800

    12 - Migration Basics Part 1

--- src/products/models.py
+++ src/products/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Product(models.Model):
 	title = models.CharField(max_length=30) #owiuerpoajsdlfkjasd;flkiu1p3o4u134123 ewjfa;sd
+	description = models.TextField()
+	price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99) #100.00
 
 	def __unicode__(self): #def __unicode__(self):
 		return self.title
