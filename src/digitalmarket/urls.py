f221891ac9565d7802537d629d83ee25b50680e9
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-11-16 14:38:44 -0800

    17 - Urls

--- src/digitalmarket/urls.py
+++ src/digitalmarket/urls.py
@@ -17,5 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),    
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^detail/$', 'products.views.detail_view', name='detail_view'),
+    url(r'^list/$', 'products.views.list_view', name='list_view'),
+    
 ]
