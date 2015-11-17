86d5052498a073ef92af65bf1688b66371e7dfdb
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-11-17 13:08:28 -0800

    21 - Dynamic URL Patters

--- src/digitalmarket/urls.py
+++ src/digitalmarket/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^detail/$', 'products.views.detail_view', name='detail_view'),
+    url(r'^detail/(?P<object_id>\d+)/$', 'products.views.detail_view', name='detail_view'),
     url(r'^list/$', 'products.views.list_view', name='list_view'),
     
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,22 +1,27 @@
-from django.shortcuts import render
-
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 from .models import Product
 
-def detail_view(request):
-	# 1 item
-	if request.user.is_authenticated():
-		product = Product.objects.all().first()
-		template = "detail_view.html"
-		context = {
-			"object": product
+def detail_view(request, object_id=None):
+	product = get_object_or_404(Product, id=object_id)
+	template = "detail_view.html"
+	context = {
+		"object": product
 		}
-	else:
-		template = "not_found.html"
-		context = {}
 	return render(request, template, context)
+	# if object_id is not None:
+	# 	product = get_object_or_404(Product, id=object_id)
+	# 	# product = Product.objects.get(id=object_id)
+	# 	# try:
+	# 	# 	product = Product.objects.get(id=object_id)
+	# 	# except Product.DoesNotExist:
+	# 	# 	product = None
 
+		
+	# else:
+	# 	raise Http404
 
 
 def list_view(request):
