08775c5dd1a1b19f9e222ed5e10d175273067220
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-11-17 15:07:04 -0800

    28 - Model Form

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 
+
+
+from .models import Product
+
 PUBLISH_CHOICES = (
 	#('', ""),
 	('publish', "Publish"),
 	('draft', "Draft"),
 )
+
 class ProductAddForm(forms.Form):
 	title = forms.CharField(label='Your Title', widget= forms.TextInput(
 		attrs={
@@ -37,3 +42,71 @@ class ProductAddForm(forms.Form):
 			return title
 		else:
 			raise forms.ValidationError("Title must be greater than 3 characters long.")
+
+
+
+class ProductModelForm(forms.ModelForm):
+	publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
+	# description = forms.CharField(widget=forms.Textarea(
+	# 		attrs={
+	# 			"class": "my-custom-class",
+	# 			"placeholder": "Description",
+	# 			"some-attr": "this",
+	# 		}
+	# ))
+	class Meta:
+		model = Product
+		fields = [
+			"title",
+			"description",
+			"price",
+		]
+		widgets = {
+			"description": forms.Textarea(
+					attrs={
+						"placeholder": "New Description"
+					}
+				),
+			"title": forms.TextInput(
+				attrs= {
+					"placeholder": "Title"
+				}
+			)
+		}
+
+
+	def clean_price(self):
+		price = self.cleaned_data.get("price")
+		if price <= 1.00:
+			raise forms.ValidationError("Price must be greater than $1.00")
+		elif price >= 99.99:
+			raise forms.ValidationError("Price must be less than $100.00")
+		else:
+			return price
+
+	def clean_title(self):
+		title = self.cleaned_data.get("title")
+		if len(title) > 3:
+			return title
+		else:
+			raise forms.ValidationError("Title must be greater than 3 characters long.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/products/views.py
+++ src/products/views.py
@@ -2,26 +2,17 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
-from .forms import ProductAddForm
+from .forms import ProductAddForm, ProductModelForm
 from .models import Product
 
 
 def create_view(request): 
-	# FORM
-	form = ProductAddForm(request.POST or None)
-	# if request.method == "POST":
-	# 	print request.POST.get("price")
+	form = ProductModelForm(request.POST or None)
 	if form.is_valid():
-		data = form.cleaned_data
-		title = data.get("title")
-		description = data.get("description")
-		price = data.get("price")
-		#new_obj = Product.objects.create(title=title, description=description, price=price)
-		new_obj = Product()
-		new_obj.title = title
-		new_obj.description = description
-		new_obj.price = price
-		new_obj.save()
+		print form.cleaned_data.get("publish")
+		instance = form.save(commit=False)
+		instance.sale_price = instance.price
+		instance.save()
 	template = "create_view.html"
 	context = {
 			"form": form,
