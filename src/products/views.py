27f27138298a8ff5fc054708dad0b3622b640dad
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-11-18 13:49:35 -0800

    32 - Multiple Objects Error with Slugs

--- src/products/views.py
+++ src/products/views.py
@@ -13,7 +13,17 @@ from .models import Product
 class ProductDetailView(DetailView):
 	model = Product
 
-
+	def get_object(self, *args, **kwargs):
+		slug = self.kwargs.get("slug")
+		ModelClass = self.model
+		if slug is not None:
+			try:
+				obj = get_object_or_404(ModelClass, slug=slug)
+			except ModelClass.MultipleObjectsReturned:
+				obj = ModelClass.objects.filter(slug=slug).order_by("-title").first()
+		else:
+			obj = super(ProductDetailView, self).get_object(*args, **kwargs)
+		return obj
 
 class ProductListView(ListView):
 	model = Product
