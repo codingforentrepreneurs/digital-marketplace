cc5277db971198c30175817890de6d4e2732cd96
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-11-18 12:56:54 -0800

    30 - Include Template Tag

--- src/products/views.py
+++ src/products/views.py
@@ -13,9 +13,10 @@ def create_view(request):
 		instance = form.save(commit=False)
 		instance.sale_price = instance.price
 		instance.save()
-	template = "create_view.html"
+	template = "form.html"
 	context = {
 			"form": form,
+			"submit_btn": "Create Product"
 		}
 	return render(request, template, context)
 
@@ -27,10 +28,11 @@ def update_view(request, object_id=None):
 		instance = form.save(commit=False)
 		#instance.sale_price = instance.price
 		instance.save()
-	template = "update_view.html"
+	template = "form.html"
 	context = {
 		"object": product,
 		"form": form,
+		"submit_btn": "Update Product"
 		}
 	return render(request, template, context)
 
