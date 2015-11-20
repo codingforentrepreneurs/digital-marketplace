c0a4aee4c88eeea01cc9a11691b9debc50a2aa6b
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-11-19 17:23:08 -0800

    42 - Setup Staticfiles

--- src/digitalmarket/settings.py
+++ src/digitalmarket/settings.py
@@ -116,3 +116,33 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#TEAM MANAGERS
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
+#TEAM DOESN'T
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "staticfiles") #'collect files here' #CDN / AWS S3 Bucket/ 
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
