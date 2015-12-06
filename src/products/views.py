5b93b4d75663e5932ecc28516906d26812c798f9
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-12-06 12:51:34 -0800

    88 - Star Ratings with CSS

--- src/products/views.py
+++ src/products/views.py
@@ -94,8 +94,9 @@ class ProductDetailView(MultiSlugMixin, DetailView):
 		context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
 		obj = self.get_object()
 		tags = obj.tag_set.all()
-		for tag in tags:
-			new_view = TagView.objects.add_count(self.request.user, tag)
+		if self.request.user.is_authenticated():
+			for tag in tags:
+				new_view = TagView.objects.add_count(self.request.user, tag)
 		return context
 
 
